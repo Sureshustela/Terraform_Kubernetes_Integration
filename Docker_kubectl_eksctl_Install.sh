1886389d676b6aeb10b36a51f7822c6c7d7f927c
SureshU <sureshdevops@gmail.com>
2026-01-25 14:30:32 +0530

    script file changed

--- Docker_kubectl_eksctl_Install.sh
+++ Docker_kubectl_eksctl_Install.sh
@@ -52,9 +52,9 @@ tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
 
-# Verify installations
+# Verify installations 
 docker --version
-kubectl version --client --short
+kubectl version --client
 eksctl version  
 
 echo "Docker, kubectl, and eksctl have been installed successfully."
