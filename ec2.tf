33821d3d1d6754f7777e1f3b2e251e7663e1d2df
SureshU <sureshdevops@gmail.com>
2026-02-02 10:02:15 +0530

    latest

--- ec2.tf
+++ ec2.tf
@@ -56,6 +56,13 @@ resource "aws_security_group" "ssh22" {
   cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  to_port = 80
+  from_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress  {
   to_port = 0
   from_port = 0
